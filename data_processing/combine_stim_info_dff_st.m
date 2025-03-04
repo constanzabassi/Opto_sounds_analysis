a9a2015d3de419b52dc2c5998d924d6264c0eb77
constanzabassi <cdb66@pitt.edu>
2025-03-04 13:22:56 -0500

    update to include zscored

--- data_processing/combine_stim_info_dff_st.m
+++ data_processing/combine_stim_info_dff_st.m
@@ -1,13 +1,40 @@
-function [stim_trials_combined, dff_combined] = combine_stim_info_dff_st(stim_info1, stim_info2,dff1, dff2)
-stim_trials_combined = {};
-for m = 1:length(stim_info1)
-    stim_trials_combined{m,1} = [stim_info1{m,1};stim_info2{m,1}];
-    stim_trials_combined{m,2} = [stim_info1{m,2},stim_info2{m,2}+length(stim_info1{m,1})];
-    stim_trials_combined{m,3} = [stim_info1{m,3},stim_info2{m,3}+length(stim_info1{m,1})];
+function [stim_trials_combined, dff_combined] = combine_stim_info_dff_st(stim_info1, stim_info2, dff1, dff2)
+% COMBINE_STIM_INFO_DFF_ST vertically stacks two contexts together 
+% and keeps track of trial indices while combining dF/F data.
+%
+% Inputs:
+%   stim_info1 - Cell array containing stimulus info for the first context.
+%   stim_info2 - Cell array containing stimulus info for the second context.
+%   dff1       - Cell array containing dF/F data for the first context.
+%   dff2       - Cell array containing dF/F data for the second context.
+%
+% Outputs:
+%   stim_trials_combined - Cell array with combined stimulus trial indices.
+%   dff_combined         - Structure array with merged dF/F data.
+%
+% Author: CB 03/04/2025
 
-    dff_combined{1,m}.stim = [dff1{1,m}.stim; dff2{1,m}.stim];
-    dff_combined{1,m}.ctrl = [dff1{1,m}.ctrl; dff2{1,m}.ctrl];
-%     dff_combined{1,m}.z_stim = [dff1{1,m}.z_stim; dff2{1,m}.z_stim];
-%     dff_combined{1,m}.z_ctrl = [dff1{1,m}.z_ctrl; dff2{1,m}.z_ctrl];
+stim_trials_combined = {};  % Initialize combined stimulus trials structure
+
+for current_datast = 1:length(stim_info1)
+    % Combine stimulus trial indices
+    stim_trials_combined{current_datast,1} = [stim_info1{current_datast,1}; stim_info2{current_datast,1}];
+    
+    % Adjust trial indices to prevent overlap
+    stim_trials_combined{current_datast,2} = [stim_info1{current_datast,2}, ...
+                                              stim_info2{current_datast,2} + length(stim_info1{current_datast,1})]; %stim trials
+    stim_trials_combined{current_datast,3} = [stim_info1{current_datast,3}, ...
+                                              stim_info2{current_datast,3} + length(stim_info1{current_datast,1})]; %control trials
+
+    % Combine dF/F data for stimulus and control conditions
+    dff_combined{1, current_datast}.stim = [dff1{1, current_datast}.stim; dff2{1, current_datast}.stim];
+    dff_combined{1, current_datast}.ctrl = [dff1{1, current_datast}.ctrl; dff2{1, current_datast}.ctrl];
+
+    % Ensure z-scored data exists before merging
+    if isfield(dff1{1, current_datast}, 'z_stim') && isfield(dff2{1, current_datast}, 'z_stim')
+        dff_combined{1, current_datast}.z_stim = [dff1{1, current_datast}.z_stim; dff2{1, current_datast}.z_stim];
+        dff_combined{1, current_datast}.z_ctrl = [dff1{1, current_datast}.z_ctrl; dff2{1, current_datast}.z_ctrl];
+    end
+end
 
 end
--- data_processing/organize_2context.m
+++ data_processing/organize_2context.m
@@ -1,24 +1,65 @@
-function [dff_context,stim_trials_context,ctrl_trials_context] = organize_2context(contex_st,context2_st,varargin)
-for m = 1:length(contex_st)
+function [dff_context, stim_trials_context, ctrl_trials_context] = organize_2context(context_st, context2_st, varargin)
+% ORGANIZE_2CONTEXT organizes neural activity data for two different contexts.
+% It extracts and structures stimulus (stim) and control (ctrl) trial data 
+% from two context structures and ensures trial indices are managed properly.
+%
+% Inputs:
+%   context_st  - Cell array containing neural data for the first context.
+%   context2_st - Cell array containing neural data for the second context.
+%   varargin    - Optional additional contexts (not currently used but allows extension).
+%
+% Outputs:
+%   dff_context         - Structure array with organized neural data.
+%   stim_trials_context - Cell array of stimulus trial indices for each dataset.
+%   ctrl_trials_context - Cell array of control trial indices for each dataset.
+%
+% Author: CB 03/04/2025
+
+for current_dataset = 1:length(context_st)
     condition1_trials_all = [];
     condition2_trials_all = [];
-    for c = 1:nargin
-        if c == 1
-            stim_trials = 1:size(contex_st{1,m}.stim,1);
-            dff_context{c,m}.stim = contex_st{1,m}.stim;
-            ctrl_trials = 1:size(contex_st{1,m}.ctrl,1);
-            dff_context{c,m}.ctrl = contex_st{1,m}.ctrl;
-            condition1_trials_all = [condition1_trials_all,{stim_trials}];
-            condition2_trials_all = [condition2_trials_all,{ctrl_trials}];
-        else
-            stim_trials = stim_trials(end)+1:stim_trials(end)+size(context2_st{1,m}.stim,1);
-            dff_context{c,m}.stim = context2_st{1,m}.stim;
-            ctrl_trials = ctrl_trials(end)+1:ctrl_trials(end)+size(context2_st{1,m}.ctrl,1);
-            dff_context{c,m}.ctrl = context2_st{1,m}.ctrl;
-            condition1_trials_all = [condition1_trials_all,{stim_trials}];
-            condition2_trials_all = [condition2_trials_all,{ctrl_trials}];
+    
+    for current_context = 1:nargin
+        if current_context == 1  % First context
+            % Get trial indices
+            stim_trials = 1:size(context_st{1, current_dataset}.stim, 1);
+            ctrl_trials = 1:size(context_st{1, current_dataset}.ctrl, 1);
+            
+            % Store data in output structure
+            dff_context{current_context, current_dataset}.stim = context_st{1, current_dataset}.stim;
+            dff_context{current_context, current_dataset}.ctrl = context_st{1, current_dataset}.ctrl;
+            
+            % Store trial indices
+            condition1_trials_all = [condition1_trials_all, {stim_trials}];
+            condition2_trials_all = [condition2_trials_all, {ctrl_trials}];
+            
+            % Check if z-scored data exists and store it
+            if isfield(context_st{1, current_dataset}, 'z_stim') && isfield(context_st{1, current_dataset}, 'z_ctrl')
+                dff_context{current_context, current_dataset}.z_stim = context_st{1, current_dataset}.z_stim;
+                dff_context{current_context, current_dataset}.z_ctrl = context_st{1, current_dataset}.z_ctrl;
+            end
+        else  % Second context (or additional contexts)
+            % Adjust trial indices to avoid overlap
+            stim_trials = stim_trials(end) + (1:size(context2_st{1, current_dataset}.stim, 1));
+            ctrl_trials = ctrl_trials(end) + (1:size(context2_st{1, current_dataset}.ctrl, 1));
+            
+            % Store data in output structure
+            dff_context{current_context, current_dataset}.stim = context2_st{1, current_dataset}.stim;
+            dff_context{current_context, current_dataset}.ctrl = context2_st{1, current_dataset}.ctrl;
+            
+            % Store trial indices
+            condition1_trials_all = [condition1_trials_all, {stim_trials}];
+            condition2_trials_all = [condition2_trials_all, {ctrl_trials}];
+            
+            % Check if z-scored data exists and store it
+            if isfield(context2_st{1, current_dataset}, 'z_stim') && isfield(context2_st{1, current_dataset}, 'z_ctrl')
+                dff_context{current_context, current_dataset}.z_stim = context2_st{1, current_dataset}.z_stim;
+                dff_context{current_context, current_dataset}.z_ctrl = context2_st{1, current_dataset}.z_ctrl;
+            end
         end
     end
-    stim_trials_context{m} = condition1_trials_all;
-    ctrl_trials_context{m} = condition2_trials_all;
+    
+    % Store trial indices for both conditions
+    stim_trials_context{current_dataset} = condition1_trials_all;
+    ctrl_trials_context{current_dataset} = condition2_trials_all;
 end
