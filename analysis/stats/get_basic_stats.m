21d597c733aa52b066a2b02397d3ef596fcadffe
constanzabassi <cdb66@pitt.edu>
2025-03-20 11:33:24 -0400

    minor update basic_stats

--- analysis/stats/get_basic_stats.m
+++ analysis/stats/get_basic_stats.m
@@ -4,6 +4,11 @@ basic_stats.sd = std(data1,'omitnan');
 % basic_stats.ci = paramci(data1);
 basic_stats.n = length(data1);
 basic_stats.n_nonan = length(data1(~isnan(data1)));
-[ci,bootstat] = bootci(1000,@(x)[mean(x,'omitnan') std(x,'omitnan')],data1); %ci (:,1) lower and upper bounds of mean, ci (:,2) lower and uppder bounds of standard deviation
-basic_stats.ci = ci;
-basic_stats.bootstat = bootstat;
+if length(data1) > 1
+    [ci,bootstat] = bootci(1000,@(x)[mean(x,'omitnan') std(x,'omitnan')],data1); %ci (:,1) lower and upper bounds of mean, ci (:,2) lower and uppder bounds of standard deviation
+    basic_stats.ci = ci;
+    basic_stats.bootstat = bootstat;
+else
+    basic_stats.ci = nan;
+    basic_stats.bootstat = nan;
+end
